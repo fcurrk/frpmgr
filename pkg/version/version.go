256ac8c2d71cd9f8460d9fa3c14476895e38f040
fcurrk <48197714+fcurrk@users.noreply.github.com>
2024-03-11 16:06:26 +0800

    Update version.go

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -1,13 +1,9 @@
 package version
 
-import (
-	"github.com/fatedier/frp/pkg/util/version"
-)
-
 var (
 	Number = "1.14.2"
 	// FRPVersion is the version of FRP used by this program
-	FRPVersion = version.Full()
+	FRPVersion = "0.49.0"
 	// BuildDate is the day that this program was built
 	BuildDate = ""
 )
