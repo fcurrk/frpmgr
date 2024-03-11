5e19151f2553fadf2ce74aaf648cae81dcbdd93a
fcurrk <48197714+fcurrk@users.noreply.github.com>
2024-03-11 16:18:28 +0800

    update

--- generate.go
+++
@@ -1,4 +0,0 @@
-package main
-
-//go:generate go run iconize.go -d icon
-//go:generate go run resource.go
--- pkg/version/version.go
+++
@@ -1,9 +0,0 @@
-package version
-
-var (
-	Number = "1.14.2"
-	// FRPVersion is the version of FRP used by this program
-	FRPVersion = "0.49.0"
-	// BuildDate is the day that this program was built
-	BuildDate = ""
-)
